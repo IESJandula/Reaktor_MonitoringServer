cdde1242fc9730f2204e0a87eeafe364626de39f
DavidMartinezFlores <dmarflo526@g.educaand.es>
2024-02-19 12:43:15 +0100

    ADD SERVER STRUCTURE CHECK METHOD

--- src/main/java/es/iesjandula/reaktor/monitoring_server/MonitoringServerApplication.java
+++ src/main/java/es/iesjandula/reaktor/monitoring_server/MonitoringServerApplication.java
@@ -1,5 +1,7 @@
 package es.iesjandula.reaktor.monitoring_server;
 
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,10 +11,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import es.iesjandula.reaktor.monitoring_server.interfaces.IParseActions;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @EnableScheduling
 @EntityScan(basePackages = "es.iesjandula.reaktor.models")
+@Slf4j
 public class MonitoringServerApplication implements CommandLineRunner
 {
 	
@@ -28,8 +32,41 @@ public class MonitoringServerApplication implements CommandLineRunner
 	@Transactional
 	public void run(String... args) throws Exception
 	{
-
+		this.checkServerStructure();
 		this.iParseActions.parseFile(".\\src\\main\\resources\\actionsCSV.csv");
 		
 	}
+    /**
+     * Method checkClientStructure
+     */
+    public void checkServerStructure() 
+    {
+    	File fileFolder = new File("."+File.separator+"files");
+    	log.info(fileFolder.getAbsolutePath()+"-> EXIST fileFolder:"+fileFolder.exists());
+    	if(!fileFolder.exists()) 
+    	{
+    		fileFolder.mkdir();
+    	}
+    	
+    	File wifiFolder = new File("."+File.separator+"confWIFI");
+    	log.info(wifiFolder.getAbsolutePath()+"-> EXIST wifiFolder :"+wifiFolder.exists());
+    	if(!wifiFolder.exists()) 
+    	{
+    		wifiFolder.mkdir();
+    	}
+    	
+    	File screenshotsFolder = new File("."+File.separator+"screenshots");
+    	log.info(screenshotsFolder.getAbsolutePath()+"-> EXIST screenshotsFolder :"+screenshotsFolder.exists());
+    	if(!screenshotsFolder.exists()) 
+    	{
+    		screenshotsFolder.mkdir();
+    	}
+    	
+    	File webScreenshotsFolder = new File("."+File.separator+"webScreenshots");
+    	log.info(webScreenshotsFolder.getAbsolutePath()+"-> EXIST webScreenshotsFolder :"+webScreenshotsFolder.exists());
+    	if(!webScreenshotsFolder.exists()) 
+    	{
+    		webScreenshotsFolder.mkdir();
+    	}
+    }
 }
